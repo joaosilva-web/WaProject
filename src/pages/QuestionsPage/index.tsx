f6b386f91f5ae8ecd049d609ecd64dc0bd9a94ef
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-23 13:32:50 -0300

    add new Feature: now the questions and answers appear on the screen

--- src/pages/QuestionsPage/index.tsx
+++ src/pages/QuestionsPage/index.tsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
-
+interface ApiReceived {
+  response_code: number;
+  results: [Question];
+}
+interface Question {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: [string];
+}
 
 export function QuestionsPage() {
-    return(
-        <h1>QuestionsPage</h1>
-    )
+  const [questions, setQuestions] = useState<Question[]>([] as Question[]);
+
+  useEffect(() => {
+    async function loadQuest() {
+      const { data } = await api.get<ApiReceived>(
+        `api.php?amount=${localStorage.getItem("amountQuestions")}`
+      );
+
+      if (data) {
+        console.log(data);
+        const newQuestions = [...data.results];
+        setQuestions(newQuestions);
+      }
+    }
+    loadQuest();
+    localStorage.removeItem("amountQuestions");
+  }, []);
+
+  return (
+    <main>
+      <form>
+        {questions.map((question) => (
+          <label key={question.question}>
+            <h1 dangerouslySetInnerHTML={{ __html: question.question }}></h1>
+            {question.incorrect_answers
+              .concat(question.correct_answer)
+              .map((answers) => (
+                <label key={answers} htmlFor={answers}>
+                  <input type="radio" value={answers} id={answers} />
+                  <p>{answers}</p>
+                </label>
+              ))}
+          </label>
+        ))}
+        <button>Terminar</button>
+      </form>
+    </main>
+  );
 }
