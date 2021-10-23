32e72a8ca6d34cc3ab0b9dc179bf356788573439
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-23 12:48:42 -0300

    Add new Feature: Now the user can choose how many questions he wants to answer

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,39 @@
+import {  FormEvent ,useState } from "react"
+import { useHistory } from "react-router";
 
 
 
 export function HomePage() {
+    const [value, setValue] = useState("");
+    const [amount, setAmount] = useState("");
+    let continuar;
+    let history = useHistory();
+    
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        localStorage.setItem("amountQuestions", amount);
+        continueVerify()
+    }
+
+    function continueVerify() {
+            continuar = confirm("Você deseja continuar?");
+            if(continuar == true){
+                history.push("/questions")
+            }else {
+                setValue("");
+                localStorage.removeItem("amountQuestions");
+                return;
+            }
+    }
+
     return(
-        <h1>HomePage</h1>
+        <main>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <input type="number" onChange={event => setValue(event.target.value)} value={value}/>
+                <button onClick={() => setAmount(value)}>
+                    Ir Para as Questões
+                </button>
+            </form>
+        </main>
     )
 }
